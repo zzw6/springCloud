f24b4ee9b1602ce51c17b62186ae92eaf2867077
吴彦祖 <454467197@qq.com>
2020-04-23 00:12:27 +0800

    支付模块构建(中)

---
+++ payment/src/main/java/com/javalei/springcolud/paymentMain.java
@@ -0,0 +1,15 @@
+package com.javalei.springcolud;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * User: zzw
+ * Date: 2020-04-23-0:09
+ * Description: 主程序入口
+ */
+public class paymentMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(paymentMain.class,args);
+    }
+}
